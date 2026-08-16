120cc20ef90e927378f62cfd32b4acaf875a95f1
Monika <250406monika@gmail.com>
2026-08-16 23:23:36 +0530

    Time: 30 ms (67.17%), Space: 59.3 MB (12.94%) - LeetHub

--- 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.java
+++ 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.java
@@ -1,7 +1,34 @@
 class Solution {
+    class pair{
+        int dist ;
+        int index;
+        pair(int f,int s){
+            this.dist = f;
+            this.index = s;
+        }
+    }
     public int[][] kClosest(int[][] points, int k) {
-        Arrays.sort(points, (a, b) -> 
-            (a[0]*a[0] + a[1]*a[1]) - (b[0]*b[0] + b[1]*b[1]));
-        return Arrays.copyOfRange(points, 0, k);
+        
+        PriorityQueue<pair> pq = new PriorityQueue<>((a,b)-> b.dist - a.dist);
+        for(int i=0;i<points.length;i++){
+            int distance = (points[i][0]*points[i][0]+points[i][1]*points[i][1]);
+            pair curr = new pair(distance,i);
+            if(pq.size()<k){
+                pq.add(curr);
+            }else if(curr.dist<pq.peek().dist){
+                pq.poll();
+                pq.add(curr);
+            }
+        }
+        int [][]res = new int[k][points[0].length];
+        
+        int id =0;
+        while(!pq.isEmpty()){
+            int index = pq.poll().index;
+            res[id][0]= points[index][0];
+            res[id][1] = points[index][1];
+            id++;
+        }
+        return res;
     }
 }
