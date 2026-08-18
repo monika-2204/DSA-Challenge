8e983cefd7110dd5118a07c937bc8c1199e3715d
Monika <250406monika@gmail.com>
2026-08-19 01:45:11 +0530

    Time: 1 ms (99.3%), Space: 43.9 MB (99.3%) - LeetHub

--- 3471-find-the-largest-almost-missing-integer/3471-find-the-largest-almost-missing-integer.java
+++ 3471-find-the-largest-almost-missing-integer/3471-find-the-largest-almost-missing-integer.java
@@ -1,36 +1,36 @@
 class Solution {
     public int largestInteger(int[] nums, int k) {
         int n = nums.length;
-        Map<Integer, Integer> windowFreq = new HashMap<>();
-        Map<Integer, Integer> globalCount = new HashMap<>();
-
-        for (int i = 0; i < k; i++) {
-            windowFreq.put(nums[i], windowFreq.getOrDefault(nums[i], 0) + 1);
+        int freq[] = new int[51];
+        for(int num:nums){
+            freq[num]++;
         }
-        for (int num : windowFreq.keySet()) {
-            globalCount.put(num, globalCount.getOrDefault(num, 0) + 1);
-        }
-
-        for (int i = k; i < n; i++) {
-            int out = nums[i - k];
-            int in = nums[i];
-
-            windowFreq.put(out, windowFreq.get(out) - 1);
-            if (windowFreq.get(out) == 0) windowFreq.remove(out);
-
-            windowFreq.put(in, windowFreq.getOrDefault(in, 0) + 1);
-
-            for (int num : windowFreq.keySet()) {
-                globalCount.put(num, globalCount.getOrDefault(num, 0) + 1);
+        if(k==n){
+            int max = -1;
+            for(int num:nums){
+                max = Math.max(num,max);
             }
+            return max;
         }
-
+        if(k==1){
+            int a = -1;
+            for(int num:nums){
+                if(freq[num]==1){
+                    a = Math.max(num,a);
+                }
+            }
+            return a;
+        }
         int ans = -1;
-        for (int num : globalCount.keySet()) {
-            if (globalCount.get(num) == 1) {
-                ans = Math.max(ans, num);
+        if(k<n && k>1){
+            if(freq[nums[0]]==1){
+                ans = Math.max(ans,nums[0]);
+            }
+            if(freq[nums[n-1]]==1){
+                ans = Math.max(ans,nums[n-1]);
             }
         }
+
         return ans;
     }
 }
