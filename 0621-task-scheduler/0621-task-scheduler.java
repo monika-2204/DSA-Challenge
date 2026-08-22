6d36ff589c148fcb3b2b12a28ea1a84656709d0b
Monika <250406monika@gmail.com>
2026-08-22 23:57:57 +0530

    Time: 522 ms (5.02%), Space: 48.3 MB (51.86%) - LeetHub

--- 0621-task-scheduler/0621-task-scheduler.java
+++ 0621-task-scheduler/0621-task-scheduler.java
@@ -1,21 +1,58 @@
 class Solution {
+    class pair {
+        int freq;
+        char task;
+        pair(int f, char t) {
+            this.freq = f;
+            this.task = t;
+        }
+    }
+
     public int leastInterval(char[] tasks, int n) {
-        int[] freq = new int[26];
-        for (char c : tasks) freq[c - 'A']++;
-        PriorityQueue<Integer> pq = new PriorityQueue<>((a, b) -> b - a);
-        for (int f : freq) if (f > 0) pq.add(f);
+        HashMap<Character, Integer> map = new HashMap<>();
+        for (char c : tasks) {
+            map.put(c, map.getOrDefault(c, 0) + 1);
+        }
+
+        PriorityQueue<pair> pq = new PriorityQueue<>((a, b) -> b.freq - a.freq);
+        for (char ch : map.keySet()) {
+            pq.add(new pair(map.get(ch), ch));
+        }
+
         int time = 0;
+        int[] free = new int[26]; // cooldown tracker
+
         while (!pq.isEmpty()) {
-            List<Integer> temp = new ArrayList<>();
-            int cycle = n + 1;
-            while (cycle > 0 && !pq.isEmpty()) {
-                int curr = pq.poll();
-                if (curr - 1 > 0) temp.add(curr - 1);
-                time++;
-                cycle--;
+            ArrayList<pair> pulled = new ArrayList<>();
+            boolean executed = false;
+
+            while (!pq.isEmpty()) {
+                pair p = pq.poll();
+                int freq = p.freq;
+                char task = p.task;
+
+                if (free[task - 'A'] <= time) {
+                    // execute task
+                    if (freq > 1) {
+                        pulled.add(new pair(freq - 1, task));
+                    }
+                    free[task - 'A'] = time + n + 1;
+                    executed = true;
+                    break;
+                } else {
+                    pulled.add(p);
+                }
             }
-            for (int t : temp) pq.add(t);
-            if (!pq.isEmpty()) time += cycle;
+
+            for (pair p : pulled) {
+                pq.add(p);
+            }
+
+            time++;
+            if (!executed && !pq.isEmpty()) {
+                // idle time
+                continue;
+            }
         }
         return time;
     }
