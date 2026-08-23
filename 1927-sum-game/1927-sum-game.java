b3f3e16295f29b4bdda7baa8cd2d4e59bf1aeb6a
Monika <250406monika@gmail.com>
2026-08-23 22:15:26 +0530

    Time: 8 ms (53.28%), Space: 47.2 MB (10.95%) - LeetHub

---
+++ 1927-sum-game/1927-sum-game.java
@@ -0,0 +1,30 @@
+class Solution {
+    public boolean sumGame(String num) {
+        int n = num.length();
+        int first = n/2;
+
+        int leftsum = 0;
+        int leftcount = 0;
+        for(int i=0;i<first;i++){
+            if(num.charAt(i)=='?'){
+                leftcount++;
+            }else
+            leftsum += num.charAt(i) - '0';
+        }
+
+        int rightsum = 0;
+        int rightcount = 0;
+        for(int i=first;i<n;i++){
+            if(num.charAt(i)=='?'){
+                rightcount++;
+            }else
+            rightsum += num.charAt(i) - '0';
+        }
+        if((rightcount+leftcount) % 2 != 0){
+            return true;
+        }
+        int diff = leftsum-rightsum;
+        int qdiff = rightcount - leftcount;
+        return diff != (qdiff * 9)/2;
+    }
+}
