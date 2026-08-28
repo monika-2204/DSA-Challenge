81cdb9204e902adc2817bbe6d0e06e434173644d
Monika <250406monika@gmail.com>
2026-08-28 23:38:31 +0530

    Time: 1 ms (85.86%), Space: 44.4 MB (73.09%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.java
+++ 0022-generate-parentheses/0022-generate-parentheses.java
@@ -1,26 +1,27 @@
 import java.util.*;
 
 class Solution {
+
+     private void backtrack(List<String> res, StringBuilder temp, int open, int close, int n) {
+       if(open==n && close ==n){
+        res.add(temp.toString());
+        return ;
+       }
+       if(open<n){
+        temp.append('(');
+        backtrack(res,temp,open+1,close,n);
+        temp.deleteCharAt(temp.length()-1);
+       }
+       if(close<open){
+        temp.append(')');
+        backtrack(res,temp,open,close+1,n);
+        temp.deleteCharAt(temp.length()-1);
+       }
+}
+
     public List<String> generateParenthesis(int n) {
         List<String> ans = new ArrayList<>();
         backtrack(ans, new StringBuilder(), 0, 0, n);
         return ans;
-    }
-
-    private void backtrack(List<String> ans, StringBuilder cur, int open, int close, int n) {
-        if (cur.length() == 2 * n) {
-            ans.add(cur.toString());
-            return;
-        }
-        if (open < n) {
-            cur.append('(');
-            backtrack(ans, cur, open + 1, close, n);
-            cur.deleteCharAt(cur.length() - 1);
-        }
-        if (close < open) {
-            cur.append(')');
-            backtrack(ans, cur, open, close + 1, n);
-            cur.deleteCharAt(cur.length() - 1);
-        }
     }
 }
