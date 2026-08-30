3975fba509f5da833701e35f874046d5175d15c6
Monika <250406monika@gmail.com>
2026-08-30 23:54:32 +0530

    Time: 2 ms (95.55%), Space: 45.8 MB (33.41%) - LeetHub

---
+++ 0039-combination-sum/0039-combination-sum.java
@@ -0,0 +1,26 @@
+class Solution {
+    void fun(int[] arr, int n, int target, List<Integer> diary, int sum, List<List<Integer>> res, int idx) {
+        if (idx == n) {
+            if (sum == target) {
+                res.add(new ArrayList<>(diary));
+            }
+            return;
+        }
+
+        fun(arr, n, target, diary, sum, res, idx + 1);
+
+        if (sum + arr[idx] <= target) {
+            diary.add(arr[idx]);
+            fun(arr, n, target, diary, sum + arr[idx], res, idx); 
+            diary.remove(diary.size() - 1); 
+        }
+    }
+
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        List<List<Integer>> list = new ArrayList<>();
+        List<Integer> diary = new ArrayList<>();
+        int n = candidates.length;
+        fun(candidates, n, target, diary, 0, list, 0);
+        return list;
+    }
+}
