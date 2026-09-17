437df463f5eb0ba717bb2f113d68b1cd5dec9990
Monika <250406monika@gmail.com>
2026-09-17 19:48:10 +0530

    Time: 1 ms (87.24%), Space: 44 MB (75.16%) - LeetHub

--- 0078-subsets/0078-subsets.java
+++ 0078-subsets/0078-subsets.java
@@ -1,13 +1,13 @@
 class Solution {
     void fun(int []nums,List<List<Integer>> ans,List<Integer> diary,int n,int idx){
-        if(idx==n){
-            ans.add(new ArrayList<>(diary));
-            return;
+        ans.add(new ArrayList<>(diary));
+          
+        for(int i=idx;i<n;i++){
+            diary.add(nums[i]);
+            fun(nums,ans,diary,n,i+1);
+            diary.remove(diary.size()-1);
         }
-        fun(nums,ans,diary,n,idx+1);
-        diary.add(nums[idx]);
-        fun(nums,ans,diary,n,idx+1);
-        diary.remove(diary.size()-1);
+        
     }
     public List<List<Integer>> subsets(int[] nums) {
         List<List<Integer>> ans = new ArrayList<>();
