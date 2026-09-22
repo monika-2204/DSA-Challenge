04663ad960bd37e06e4be6cfde3829fb440ebeac
Monika <250406monika@gmail.com>
2026-09-22 23:09:02 +0530

    Time: 3 ms (73.07%), Space: 47.5 MB (10.53%) - LeetHub

--- 0653-two-sum-iv-input-is-a-bst/0653-two-sum-iv-input-is-a-bst.java
+++ 0653-two-sum-iv-input-is-a-bst/0653-two-sum-iv-input-is-a-bst.java
@@ -14,22 +14,53 @@
  * }
  */
 class Solution {
-    List<Integer> ans = new ArrayList<>();
-    void fun(TreeNode root){
-        if(root==null) return ;
-        fun(root.left);
-        ans.add(root.val);
-        fun(root.right);
+    Stack<TreeNode> asc = new Stack<>();
+    Stack<TreeNode> desc = new Stack<>();
+    TreeNode getsmall(){
+        if(asc.isEmpty()) return null;
+        TreeNode small = asc.peek();
+        asc.pop();
+        TreeNode rightChild = small.right;
+        while(rightChild != null){
+            asc.add(rightChild);
+            rightChild = rightChild.left;
+        }
+        return small;
+    }
+    TreeNode getbig(){
+        if(desc.isEmpty()) return null;
+        TreeNode big = desc.peek();
+        desc.pop();
+        TreeNode leftChild = big.left;
+        while(leftChild != null){
+            desc.add(leftChild);
+            leftChild = leftChild.right;
+        }
+        return big;
     }
     public boolean findTarget(TreeNode root, int k) {
-        fun(root);
-        int i = 0;
-        int j = ans.size()-1;
-        while(i<j){
-            int sum = ans.get(i) + ans.get(j);
+        
+        if(root==null) return false;
+        TreeNode t = root;
+        while(t != null){
+            asc.add(t);
+            t = t.left;
+        }
+        t = root;
+        while(t != null){
+            desc.add(t);
+            t = t.right;
+        }
+        TreeNode i = getsmall();
+        TreeNode j = getbig();
+        while(i != null && j != null && i!=j && i.val<=j.val){
+            int sum = i.val + j.val;
             if(sum==k) return true;
-            else if(sum<k) i++;
-            else j--;
+            if(sum<k){
+                i = getsmall();
+            }else {
+                j = getbig();
+            }
         }
         return false;
     }
